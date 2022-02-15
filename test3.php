603d34f4d9cb20c11434953f2146a15cb3ee6e27
devgautam1 <gautamreturn@gmail.com>
2022-02-15 10:35:25 +0530

    Adding test3

---
+++ test3.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>test3</title>
+	<script type="text/javascript" src="custom.js"></script>
+</head>
+<body>
+<main>
+    
+</main>
+</body>
+</html>
